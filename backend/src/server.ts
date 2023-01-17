83eb2576924078ba767584af5a4700b37e367c43
Karine <karine.costa@somagrupo.com.br>
2023-01-17 00:23:19 -0300

    ex de retornar dados do bd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,24 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "hello!";
+app.get("/", async() => {
+  const habits =  await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'beber'
+      }
+    }
+  });
+  return habits
 });
 
-app.listen({
-  port: 3333,
-}).then(()=>{
-    console.log('rodando')
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("rodando");
+  });

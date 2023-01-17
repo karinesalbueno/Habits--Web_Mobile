14ef61f63d1e32060aa5341a6dfe88ab9d4b7846
Karine <karine.costa@somagrupo.com.br>
2023-01-17 00:56:08 -0300

    componente habit

---
+++ frontend/src/components/Habit.tsx
@@ -0,0 +1,14 @@
+import './Habit.css'
+interface HabitProps{
+    completed: number
+}
+
+export function Habit(props: HabitProps) {
+    return (
+        <div className='habit'>
+            {props.completed}
+        </div>
+    )
+}
+
+export default Habit

8abf17e347899b4b5a490675b896c746f121a02c
Karine <karine.costa@somagrupo.com.br>
2023-01-20 14:55:09 -0300

    adiciona loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Loading } from './src/assets/components/Loading';
 
 export default function App() {
   // fontsLoader: se a fonte está ou não carregada no dispositivo
   const [fontsLoader] = useFonts({
-    Inter_400Regular, 
-    Inter_600SemiBold, 
+    Inter_400Regular,
+    Inter_600SemiBold,
     Inter_700Bold,
     Inter_800ExtraBold
   });
-  
+
   //se nao estiver carregada, nao rodar a aplicação ainda
-  if (!fontsLoader){
-    return;
+  if (!fontsLoader) {
+    return (
+      <Loading />
+    )
   }
 
   return (

5158a95adc69ccc3a5a17b323115d189a6b26353
Karine <karine.costa@somagrupo.com.br>
2023-01-20 14:49:12 -0300

    fonte de texto

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
 
 export default function App() {
+  // fontsLoader: se a fonte está ou não carregada no dispositivo
+  const [fontsLoader] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+  
+  //se nao estiver carregada, nao rodar a aplicação ainda
+  if (!fontsLoader){
+    return;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>hello word!</Text>
+      <Text>hello word *-*</Text>
       <StatusBar style="auto" />
     </View>
   );

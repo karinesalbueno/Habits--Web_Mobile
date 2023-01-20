44c6caa973d92b3253f17f4ccc1cdd97652bf460
Karine <karine.costa@somagrupo.com.br>
2023-01-20 17:11:39 -0300

    [router] post - cria hábito

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,35 @@
 import { FastifyInstance } from "fastify";
+//biblioteca manipular datas
+import dayjs from "dayjs";
+//validação de schema typescript
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post("/habits", async (req) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(
+        //domingo até sábado
+        z.number().min(0).max(6)
+      ),
+    });
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+        created_at: today,
+      },
+    });
   });
 }

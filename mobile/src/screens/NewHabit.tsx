433d71239dc716419b8093f24d14bfb2ec50187c
Karine <karine.costa@somagrupo.com.br>
2023-01-21 13:09:20 -0300

    botão de seta voltar - buttonBack

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,10 +1,15 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { BackButton } from "../assets/components/BackButton";
 
 export function NewHabit() {
     return (
         <View
-            className="flex-1  bg-background ">
+            className="flex-1 bg-background px-8 pt-16 ">
+            {/* rolagem */}
+            <ScrollView showsVerticalScrollIndicator={false}>
 
+                <BackButton />
+            </ScrollView>
         </View>
     )
 }

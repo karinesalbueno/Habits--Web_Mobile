206be2f80b47af4821c4d3df08c806c827a936cc
Karine <karine.costa@somagrupo.com.br>
2023-01-21 02:04:56 -0300

    [router] patch - marca e remove hábito

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -76,4 +76,60 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits,
     };
   });
+
+  //marcar e remover um hábito
+  app.patch("/habits/:id/toggle", async (req) => {
+    const toggleHabitsParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitsParams.parse(req.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+
+    await prisma.dayHabit.create({
+      data: {
+        day_id: day.id,
+        habit_id: id,
+      },
+    });
+  });
 }

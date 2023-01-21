437028eb84df896eb3037efdcbcdb1cd70d6d349
Karine <karine.costa@somagrupo.com.br>
2023-01-21 11:49:09 -0300

    componente ProgressBar

--- frontend/src/components/Habit.tsx
+++ frontend/src/components/Habit.tsx
@@ -1,4 +1,5 @@
 import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
 
 // interface HabitProps {
 //     completed: number
@@ -17,14 +18,7 @@ export function Habit() {
                     className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
                     <span className='font-semibold text-zinc-400'> segunda-feira </span>
                     <span className='mt-1 font-bold leading-tight text-1xl'> 17/01 </span>
-
-                    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
-                        <div
-                            role={'progressbar'}
-                            aria-label="Progresso de hábitos"
-                            aria-valuenow={75}
-                            className='h-3 rounded-xl bg-violet-600 w-3/4' />
-                    </div>
+                    <ProgressBar progress={40}/>
                     <Popover.Arrow
                         height={8}
                         width={16}

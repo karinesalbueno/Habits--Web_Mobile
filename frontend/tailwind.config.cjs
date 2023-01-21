5c84547d5b238359f730bb574aff1dfaec3db59e
Karine <karine.costa@somagrupo.com.br>
2023-01-20 22:25:39 -0300

    tabela

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,13 +3,16 @@ module.exports = {
   content: [
     // estilização em todos os arquivos terminados em tsx na pasta src
     './src/**/*.tsx',
-    './index.html'
+    './index.html',
   ],
   theme: {
     extend: {
       colors: {
-        bg: '#09090A'
-      }
+        bg: '#09090A',
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0,1))',
+      },
     },
   },
   plugins: [],

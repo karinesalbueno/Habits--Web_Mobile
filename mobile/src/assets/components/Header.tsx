f549d2e678fda5dfdd7a395b41c0f51a928d241c
Karine <karine.costa@somagrupo.com.br>
2023-01-21 13:03:57 -0300

    exibe rota ao clicar no botão especifico

--- mobile/src/assets/components/Header.tsx
+++ mobile/src/assets/components/Header.tsx
@@ -1,9 +1,15 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import { useNavigation } from '@react-navigation/native'
 import { Feather } from '@expo/vector-icons';
-import colors from 'tailwindcss/colors';
+
 import Logo from '../../assets/logo.svg';
 
+import colors from 'tailwindcss/colors';
+
 export function Header() {
+
+    const { navigate } = useNavigation();
+
     return (
         <View className="w-full flex-row items-center justify-between">
             <Logo />
@@ -11,6 +17,7 @@ export function Header() {
             <TouchableOpacity
                 activeOpacity={0.7}
                 className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+                onPress={()=> navigate('newhabit')}
             >
                 <Feather
                     name='plus'

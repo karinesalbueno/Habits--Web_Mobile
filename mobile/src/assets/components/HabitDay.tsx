ced232c53de00c5afbd249e73b8770bacbb51edb
Karine <karine.costa@somagrupo.com.br>
2023-01-22 14:13:58 -0300

    [router] GET summary, passando props p/ HabitDay

--- mobile/src/assets/components/HabitDay.tsx
+++ mobile/src/assets/components/HabitDay.tsx
@@ -1,6 +1,10 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
 
-interface Props extends TouchableOpacityProps{};
+interface Props extends TouchableOpacityProps {
+    amountOfHabits?: number;
+    amountCompleted?: number;
+    date: Date;
+};
 
 //por linha 7 quadrados
 const WEEK_DAYS = 7;
@@ -9,12 +13,12 @@ const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
 export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5)
 
-export function HabitDay({...rest}: Props) {
+export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
     return (
         <View>
             <TouchableOpacity
                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
-                style={{width: DAY_SIZE, height: DAY_SIZE}}
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
                 activeOpacity={0.7}
                 {...rest}
             />
